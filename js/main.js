4564697eaf978d58ae16cc8a63681ad79b3587e6
Delaynie Wheeler <delaynie93@gmail.com>
2024-11-18 11:10:57 -0600

    updated to the top button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 // To the Top Button JS 
 // Get the button:
-let mybutton = document.getElementById("myBtn");
+let mybutton = document.getElementById("scrollToTopBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
@@ -12,12 +12,6 @@ function scrollFunction() {
   } else {
     mybutton.style.display = "none";
   }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
 document.addEventListener('DOMContentLoaded', () => {
